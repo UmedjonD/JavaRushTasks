8c9784495cc05555550f423d9cd4955fce493d82
Umedjon <Lazerr_94@mail.ru>
2019-12-14 16:49:49 +0300

    finish 16 lvl

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        Thread thread;
+
+        for (int i=0; i<5; i++) {
+            thread = new Thread(new SpecialThread());
+            list.add(thread);
+            list.get(i).run();
+        }
+
+
+        //Add your code here - добавьте свой код тут
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
